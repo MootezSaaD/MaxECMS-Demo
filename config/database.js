3c8892901c9f37f68b9ec0a8878cd23f074a8b8e
MootezSaaD <mootez.saad@medtech.tn>
2018-06-27 20:15:37 +0100

    massage

---
+++ config/database.js
@@ -0,0 +1,10 @@
+// config/database.js
+module.exports = {
+    'connection': {
+        'host': 'localhost',
+        'user': 'user',
+        'password': 'user'
+    },
+	'database': 'node_work_plz',
+    'users_table': 'users'
+};
